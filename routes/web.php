68241a5e0873488efa728e730d08acb7c3009116
Neil Alferez <neil.alferez.com>
2025-09-04 15:20:04 +0800

    Neil | 09/04/25

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// In routes/web.php
+Route::get('/student/dashboard', function () {
+    return view('student.dashboard');
+})->name('student.dashboard');
